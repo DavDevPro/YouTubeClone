f313612041987d25e7917774c5e18a28fa497581
davronbek2000 <davronbeklatibjonov@gmail.com>
2023-06-03 05:01:22 -0700

    youtube app completed

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const SearchFeed = () => {
+  const {searchTerm} = useParams();
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => setVideos(data.items));
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "#fff" }}>
+        Search Results for: <span style={{ color: "#FC1503" }}> {searchTerm}</span> videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
